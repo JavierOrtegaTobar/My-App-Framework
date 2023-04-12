a9ad824a12d536d2fb4b56a3858f7bb7e460d087
iolmedop <iolmedop@smu.cl>
2023-04-12 17:07:24 -0400

    more changes

--- app/src/main/java/com/example/myapplication/util/ExtentionMainOnClickMail.kt
+++ app/src/main/java/com/example/myapplication/util/ExtentionMainOnClickMail.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.util
 import android.app.ActivityOptions
 import android.content.Intent
 import android.text.Editable
+import android.widget.Toast
 import com.example.myapplication.LoginActivity
 import com.example.myapplication.MainActivity
 import com.example.myapplication.R
@@ -10,6 +11,8 @@ import com.example.myapplication.RegistroActivity
 
 fun MainActivity.onClickButtonEmail() {
     getUserData()
+    /*validateEmail()
+    if (dataUserCorrect) {*/
     val emailUser = email
     val intent = Intent(this, LoginActivity::class.java)
     intent.putExtra("email", emailUser)
@@ -19,6 +22,14 @@ fun MainActivity.onClickButtonEmail() {
         R.anim.slide_out_left
     ).toBundle()
     startActivity(intent, animation)
+    /*} else {
+        Toast.makeText(
+            this,
+            "El correo ingresado no existe",
+            Toast.LENGTH_SHORT
+        ).show()
+    }*/
+
 }
 
 fun MainActivity.onClickResgister() {
@@ -37,7 +48,34 @@ fun MainActivity.getUserData() {
     if (!emailUser.isNullOrEmpty()) {
         binding.txtEmail.text = Editable.Factory.getInstance().newEditable(emailUser)
         email = binding.txtEmail.text.toString()
+
+        btnOrderEnabled = true
     } else {
         email = binding.txtEmail.text.toString()
+        btnOrderEnabled = true
     }
 }
+
+fun MainActivity.buttonEnabledOrDisabled() {
+    binding.btnIngresarEmail.isEnabled =
+        (!binding.txtEmail.text.isEmpty() || !binding.txtEmail.text.isBlank()) && binding.txtEmail.text.contains(
+            "@"
+        )
+}
+
+/*
+fun MainActivity.validateEmail() {
+    val userRef = userFirebase.document(email)
+    userRef.get().addOnSuccessListener { documentSnapshot ->
+        if (documentSnapshot.exists()) {
+            val emailUser = documentSnapshot.getString("email")
+            if (emailUser == email) {
+                buttonEnabledOrDisabled()
+                dataUserCorrect = true
+            } else {
+                binding.btnIngresarEmail.isEnabled = false
+                dataUserCorrect = false
+            }
+        }
+    }
+}*/
