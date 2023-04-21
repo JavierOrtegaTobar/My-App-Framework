3f987c860d3b87a25b040979b030a6c2ff5e9113
jeortegat <jerotegat@smu.cl>
2023-04-21 10:26:42 -0400

    more changes:
    - changes to the logic when the user added the email to the main view.
    - added logic and design in menu profile.
    - implement ui firebase to the recyclerview in menu profile

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.util.buttonEnabledOrDisabled
@@ -19,25 +28,25 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
     lateinit var binding: ActivityMainBinding
     var text = 3
-    var email: String = ""
-    var btnOrderEnabled = false
+    var emailUserVar: String = ""
     val db = Firebase.firestore
     val userFirebase = db.collection("usuarios")
     var dataUserCorrect: Boolean = false
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         // splash
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getUserData()
         buttonEnabledOrDisabled()
-        binding.btnRegister.setOnClickListener(this)
-        binding.btnIngresarEmail.setOnClickListener(this)
         binding.txtEmail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
+                emailUserVar = binding.txtEmail.text.toString()
                 buttonEnabledOrDisabled()
             }
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
@@ -46,6 +55,10 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 // No se usa
             }
         })
+        binding.btnRegister.setOnClickListener(this)
+        binding.btnIngresarEmail.setOnClickListener(this)
+
+        getUserData()
     }
 
     override fun onClick(v: View?) {
